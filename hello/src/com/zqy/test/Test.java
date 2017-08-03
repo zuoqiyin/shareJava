0f2337a368b8709d2a8d1f4e8f8499e1f726c8b1
ZQY\zqy <zuoqiyin@foxmail.com>
2017-08-03 17:29:31 +0800

    新建项目

---
+++ hello/src/com/zqy/test/Test.java
@@ -0,0 +1,13 @@
+package com.zqy.test;
+
+/**
+ * Test Git
+ * @author qiyin.zuo
+ * @date 2017年8月3日
+ */
+public class Test {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Git");
+	}
+}
