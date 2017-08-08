61025d4e4c6cc1ea061f20f8cd5aa8fca5260032
左奇银 <zuoqiyin@foxmail.xom>
2017-08-08 14:13:45 +0800

    初始化

---
+++ HelloGit/src/com/zqy/test/Test.java
@@ -0,0 +1,13 @@
+package com.zqy.test;
+
+/**
+ * 测试Git Hub
+ * @author qiyin.zuo
+ * @date 2017年8月8日
+ */
+public class Test {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Git Hub!");
+	}
+}
