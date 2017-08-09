8cf5ce233e484564d72b723c3df85105bcf4491c
左奇银 <zuoqiyin@foxmail.xom>
2017-08-09 08:39:17 +0800

    项目初始化

---
+++ HelloGit/src/com/zqy/controller/DownFile.java
@@ -0,0 +1,39 @@
+package com.zqy.controller;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class DownFile {
+
+
+	@RequestMapping("/downFile.do")
+	public void downFile(HttpServletResponse response) {
+		try {
+			InputStream is = new FileInputStream("F://a.txt");
+			response.setContentType("application/octet-stream");
+            response.setHeader("Content-Disposition", "attachment;filename=" + "a.txt");
+			OutputStream os = response.getOutputStream();
+			byte[] buf = new byte[1024*1024*10];
+			int len = 0;
+			while ( (len=is.read(buf)) !=-1 ) {
+				os.write(buf,0,len);
+			}
+			os.close();
+			is.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} 
+		
+	}
+}
