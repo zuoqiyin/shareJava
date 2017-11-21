cf8714344547c05cec1f25603c96be35e93a3b19
左奇银 <zuoqiyin@foxmail.com>
2017-11-21 16:55:50 +0800

    ieda 连接测试

--- HelloGit/src/com/zqy/test/Test.java
+++ HelloGit/src/com/zqy/test/Test.java
@@ -8,6 +8,6 @@ package com.zqy.test;
 public class Test {
 
 	public static void main(String[] args) {
-		System.out.println("Hello Git Hub! v2.0");
+		System.out.println("Hello Git Hub! v2.1");
 	}
 }
