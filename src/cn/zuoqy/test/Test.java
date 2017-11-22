ed8e1d583cdffe984f150c922f71fbdf2fd8669f
左奇银 <zuoqiyin@foxmail.com>
2017-11-22 08:34:28 +0800

    更改新项目描述文件

--- src/cn/zuoqy/test/Test.java
+++ src/cn/zuoqy/test/Test.java
@@ -6,6 +6,6 @@ package cn.zuoqy.test;
 public class Test {
 
     public  static void main(String[] args) {
-        System.out.println("Hello Git -IEDA");
+        System.out.println("Hello Git -IDEA");
     }
 }
