a7f9c9b0c3b1ca938f4b122d43c37fb7a5aced84
左奇银 <zuoqiyin@foxmail.com>
2017-11-22 08:24:20 +0800

    初始化项目

---
+++ src/cn/zuoqy/test/Test.java
@@ -0,0 +1,11 @@
+package cn.zuoqy.test;
+
+/**
+ * Created by zuoqy on 8:21 2017/11/22.
+ */
+public class Test {
+
+    public  static void main(String[] args) {
+        System.out.println("Hello Git -IEDA");
+    }
+}
