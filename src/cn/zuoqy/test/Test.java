84e68b5a3ca03aad1c0740faf296d1d62eca8c9f
左奇银 <zuoqiyin@foxmail.com>
2017-12-18 10:39:41 +0800

    删除测试记录

--- src/cn/zuoqy/test/Test.java
+++ src/cn/zuoqy/test/Test.java
@@ -9,28 +9,6 @@ import java.util.List;
 public class Test {
 
     public  static void main(String[] args) {
-        List<String> list = new ArrayList<String>();
-
-        list.add(new String("1"));
-        list.add(new String("2"));
-        list.add(new String("3"));
-        list.add(new String("4"));
-        list.add(new String("5"));
-        list.add(new String("6"));
-        list.add(new String("7"));
-        list.add(new String("8"));
-        list.add(new String("9"));
-
-        for (int i=0; i<list.size(); i++) {
-            if (list.get(i).equals("9")) {
-                list.remove(i);
-                i--;
-
-            }else if (list.get(i).equals("1")) {
-                list.remove(i);
-                i--;
-            }
-        }
-        System.out.println(list);
+        System.out.println("Hello Git -IDEA");
     }
 }
