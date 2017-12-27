bdc4f637776724b1a9c05e23f4b55d9161d5bf86
左奇银 <zuoqiyin@foxmail.com>
2017-12-27 14:43:13 +0800

    增加一些测试

--- README.md
+++ README.md
@@ -4,3 +4,4 @@
 classLoad: 包中编写了java热部署的小例子
 <br/><br/>utils: 一些工具类
 <br/><br/>hiho: <a href="https://hihocoder.com">hihocoder.com<a>网站的一些code.
+<br/><br/>test: 测试类
--- src/cn/zuoqy/test/Test.java
+++ src/cn/zuoqy/test/Test.java
@@ -1,14 +1,47 @@
 package cn.zuoqy.test;
 
-import java.util.ArrayList;
-import java.util.List;
+import javax.sound.midi.Soundbank;
 
 /**
- * Created by zuoqy on 8:21 2017/11/22.
+ * Created by zuoqy on 14:05 2017/12/27.
  */
+
 public class Test {
 
-    public  static void main(String[] args) {
-        System.out.println("Hello Git -IDEA");
+    public static void main(String[] args) {
+        MyRectangle myRectangle = new MyRectangle(20.0,10.0);
+        System.out.println(myRectangle.toString());
+    }
+}
+
+interface Shape{
+     double getArea();
+
+     double getPerimeter();
+}
+
+class MyRectangle implements Shape {
+
+    private double width;
+    private double height;
+
+    public MyRectangle(double width, double height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    @Override
+    public double getArea() {
+        return this.width*this.height;
+    }
+
+    @Override
+    public double getPerimeter() {
+        return 2*(this.width+this.height);
+    }
+
+    @Override
+    public String toString() {
+        return "\"width="+this.width+",height="+height+",perimeter="+getPerimeter()+",area="+getArea()+"\"";
     }
 }
